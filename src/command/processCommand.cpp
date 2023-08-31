9cbbdbba47b5498984ecd2df85ae0aafa4a82c78
Flamiing <amirlaaouam@gmail.com>
2023-08-31 18:22:34 +0200

    some organization changes

--- src/command/processCommand.cpp
+++ src/command/processCommand.cpp
@@ -3,31 +3,21 @@
 /*                                                        :::      ::::::::   */
 /*   processCommand.cpp                                 :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: guilmira <guilmira@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alaaouam <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/22 20:10:01 by alaaouam          #+#    #+#             */
-/*   Updated: 2023/08/31 15:21:54 by guilmira         ###   ########.fr       */
+/*   Updated: 2023/08/31 18:19:46 by alaaouam         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/Server.hpp"
 
-void processCommand(Command& command)
+void processGeneralCommands(Command& command, std::map<std::string, customFunctionType>& mapCommand)
 {
-
-	std::map<std::string, customFunctionType>& 				mapCommand = command.server->mapCommand;
 	std::map<std::string, customFunctionType>::iterator		it;
 	
 	it = mapCommand.find(command.cmd);
-	if (!command.cmd.compare(PASS))
-		mapCommand[PASS](command);
-	else if (command.client->getPassAuth() == true && !command.cmd.compare(USER))
-		mapCommand[USER](command);
-	else if (command.client->getPassAuth() == true && !command.cmd.compare(NICK))
-		mapCommand[NICK](command);
-	else if (!command.cmd.compare(PONG))
-		return ;
-	else if (command.client->getAuth() == true)
+	if (command.client->getAuth() == true)
 	{
 		if (it != mapCommand.end())
 			mapCommand[command.cmd](command);
@@ -41,4 +31,21 @@ void processCommand(Command& command)
 		else
 			*command.buffer = ERR_NOTREGISTERED(command.client->getWrongNickname());
 	}
+}
+
+void processCommand(Command& command)
+{
+
+	std::map<std::string, customFunctionType>& 				mapCommand = command.server->mapCommand;
+	
+	if (!command.cmd.compare(PASS))
+		mapCommand[PASS](command);
+	else if (command.client->getPassAuth() == true && !command.cmd.compare(USER))
+		mapCommand[USER](command);
+	else if (command.client->getPassAuth() == true && !command.cmd.compare(NICK))
+		mapCommand[NICK](command);
+	else if (!command.cmd.compare(PONG))
+		return ;
+	else
+		processGeneralCommands(command, mapCommand);
 }
