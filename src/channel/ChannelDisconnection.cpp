e0a14ed3a49315f576e81b42aef1df7160782ca2
Guillermo Mira Osuna <miraosuna@usal.es>
2023-09-15 14:16:13 +0200

    2

--- src/channel/ChannelDisconnection.cpp
+++ src/channel/ChannelDisconnection.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ChannelDisconnection.cpp                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alaaouam <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: guilmira <guilmira@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/25 18:41:40 by alaaouam          #+#    #+#             */
-/*   Updated: 2023/09/08 16:17:59 by alaaouam         ###   ########.fr       */
+/*   Updated: 2023/09/15 12:58:26 by guilmira         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,9 +30,28 @@ void Channel::disconnectFromChannel(std::string client, const std::string& reply
 	_informOnlineUsers(reply);
 }
 
+std::string toLowerCase(const std::string& str)
+{
+	std::string result = str;
+	std::string::iterator it = result.begin();
+	
+	while (it != result.end())
+	{
+		*it = std::tolower(*it);
+		it++;
+	}
+	return result;
+}
+
+/* _GUILLE REVISAR LO DE LOS UPPER CASE Y NOMBRES CON MEDIO UPPER Y MEDO LOWER PORQUE TE LA PUEDEN LIAR */
+/* _GUILLE DE UNA TACADA CUANTOS PUEDO METERLE AL CANAL */
+/* _GUILLE NO TE CHEKEE PRIMERO SI ESTAS INVITED, SI NO SI YA ESTAS EN EL CHANEL */
 void Channel::removeFromChannel(std::string client, const std::string& reply)
 {
 	std::vector<Client>::iterator it = this->_onlineUsers.begin();
+	std::vector<std::string>::iterator it1 = this->_invitedUsers.begin();
+	std::string lower;
+	std::string upper;
 
 	_informOnlineUsers(reply);
 	while (it != this->_onlineUsers.end())
@@ -42,6 +61,33 @@ void Channel::removeFromChannel(std::string client, const std::string& reply)
 			this->removeTalkingPermissions(it->getNickname());
 			_removeOperator(it->getNickname());
 			this->_onlineUsers.erase(it);
+			for (std::vector<std::string>::iterator it3 = this->_invitedUsers.begin(); it3 != this->_invitedUsers.end(); it3++)
+			{
+				std::cout << *it3 << std::endl;
+			}
+			
+
+			//it1 = std::find(this->_invitedUsers.begin(), this->_invitedUsers.end(), client);
+			upper = toUpperCase(client);
+			it1 = std::remove(this->_invitedUsers.begin(), this->_invitedUsers.end(), upper);
+			std::cout << client << std::endl;
+			if (it1 != this->_invitedUsers.end())
+			{
+				this->_invitedUsers.erase(it1);
+				std::cout << "⭕⭕" << std::endl;
+				break ;
+			}
+
+			lower = toLowerCase(client);
+			it1 = std::remove(this->_invitedUsers.begin(), this->_invitedUsers.end(), lower);
+			std::cout << client << std::endl;
+			if (it1 != this->_invitedUsers.end())
+			{
+				this->_invitedUsers.erase(it1);
+				std::cout << "⭕" << std::endl;
+				break ;
+			}
+
 			break ;
 		}
 		it++;
